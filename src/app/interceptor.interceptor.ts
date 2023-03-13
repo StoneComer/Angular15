c6f9048c8ca1398294c5cba7eb6d91b688b70be3
StoneComer <fominm8@gmail.com>
2023-03-13 20:27:01 +0300

    inter3

--- src/app/interceptor.interceptor.ts
+++ src/app/interceptor.interceptor.ts
@@ -15,7 +15,7 @@ export class InterceptorInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    request = request.clone({setHeaders: {'id': '1'}})
+    request = request.clone({setHeaders: {'https://jsonplaceholder.typicode.com/todos':'1'}})
     return next.handle(request);
   }
 }
