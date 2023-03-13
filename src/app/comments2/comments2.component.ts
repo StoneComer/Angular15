ffb29aa0e3197c8c620be5899e83a0611fc57113
StoneComer <fominm8@gmail.com>
2023-03-13 23:36:29 +0300

    guard5

--- src/app/comments2/comments2.component.ts
+++ src/app/comments2/comments2.component.ts
@@ -9,9 +9,9 @@ import { DataService } from '../data.service';
 export class Comments2Component {
   constructor(public dataService:DataService){}
   comment2 = {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    userId: 1,
+    id: 2,
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
   }
 }
